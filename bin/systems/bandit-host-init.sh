26c3116273ef5228ae84f53e873ae1c56522185c
Alz <alz@phyglos.org>
2017-01-12 06:36:42 +0100

    Release 1.3.a3.2

--- bin/systems/bandit-host-init.sh
+++ bin/systems/bandit-host-init.sh
@@ -2,7 +2,7 @@
 #
 # bandit-host-init.sh - Initialize the HOST system to use BANDIT
 #
-# Copyright (C) 2015-201/ Angel Linares Zapater
+# Copyright (C) 2015-2017 Angel Linares Zapater
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as 

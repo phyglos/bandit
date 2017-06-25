b44cb8c2a768687be0669a6d699eb34c250c831c
Alz <alz@phyglos.org>
2017-06-25 10:55:11 +0200

    Show final partitions state

--- bin/systems/bandit-host-init.sh
+++ bin/systems/bandit-host-init.sh
@@ -89,7 +89,4 @@ ln -sv $BANDIT_HOST_TGT_MNT$BANDIT_BUILDER_DIR $BANDIT_BUILDER_DIR
 # Done
 bandit_log "The HOST system is ready..."
 echo
-bandit_msg "You can log as the user $BANDIT_USR to continue: " green
-echo
-bandit_msg "#su - $BANDIT_USR" green
-echo
+lsblk -ip

4d51a925d159ded1b4767085cee8844ce88b1341
Alz <alz@phyglos.org>
2017-08-31 10:14:00 +0200

    Added support for python3 modules

---
+++ lib/modules/python3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# python3 - BANDIT plugin for python3 modules
+#
+# Copyright (C) 2017 Angel Linares Zapater
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as 
+# published by the Free Software Foundation. See the COPYING file.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#
+
+module_install()
+{
+    pip3 install ${*}
+}
+
+module_remove()
+{
+    pip3 uninstall -y ${*}
+}
+

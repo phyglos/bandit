bdca4861944d721c0d8081331fbdd7955c5280e5
Alz <alz@phyglos.org>
2018-03-26 06:33:47 +0200

    Updated copyright notice

--- bin/systems/bandit-host-check.sh
+++ bin/systems/bandit-host-check.sh
@@ -2,7 +2,7 @@
 #
 # bandit-host-check.sh - Check for HOST build requirements
 #
-# Copyright (C) 2015-2017 Angel Linares Zapater
+# Copyright (C) 2015-2018 Angel Linares Zapater
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2, as 
